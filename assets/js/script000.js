98a593211bf2df44f6c303e5b3e5d8372d3327dd
Jdoe1234 <tino190991@live.dk>
2018-04-15 11:28:46 +0200

    ysuhtr

---
+++ assets/js/script000.js
@@ -0,0 +1,66 @@
+var modal = document.querySelector(".modal-bg");
+
+function setLocal(localNotes) {
+  var jsonData = JSON.stringify(localNotes);
+
+  localStorage.setItem("comment000", jsonData);
+}
+
+function getLocal() {
+  var notes = localStorage.getItem("comment000");
+
+  if(notes == null)
+  {
+    return [];
+  }
+  else {
+    return JSON.parse(notes);
+  }
+}
+
+function submitNote(text, time) {
+  var origNotes = getLocal();
+
+  var newNote = {
+    text: text,
+    dueDate: time,
+  };
+
+  origNotes.push(newNote);
+
+  setLocal(origNotes);
+}
+
+function buildList() {
+  var noteList = getLocal();
+  var ulElm = document.querySelector("ul");
+
+  ulElm.innerHTML = "";
+
+  for(var i = 0; i < noteList.length; i++) {
+    var liElm = document.createElement("li");
+    var pElm = document.createElement("p");
+
+    pElm.innerHTML = noteList[i].text;
+    liElm.appendChild(pElm);
+
+    ulElm.appendChild(liElm);
+  }
+}
+
+function submitNoteEvent(event) {
+  var noteText = document.querySelector("#noteText");
+
+  var noteTime = new Date();
+
+  submitNote(noteText.value, noteTime.value);
+  buildList();
+}
+
+window.onload = function() {
+  buildList();
+}
+
+var submitNoteBtn = document.querySelector("#addNote");
+
+submitNoteBtn.addEventListener("click", submitNoteEvent)
