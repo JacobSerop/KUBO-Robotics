4d876220bfb5ad1ed2bdfecfeaa9dead3079168a
JacobSerop <jserop@live.dk>
2018-04-16 16:26:03 +0200

    Postbutton

---
+++ assets/js/scriptjacob.js
@@ -0,0 +1,10 @@
+var modal = document.querySelector(".modul-bg");
+
+var showModalBtn = document.querySelector("#showModul");
+
+showModalBtn.addEventListener("click", function(event)
+{
+  modal.style.display = "block";
+});
+
+var submitNoteBtn = document.querySelector("#addPost");
